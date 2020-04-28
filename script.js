ecc8fc7439fa4b8b1750c7b9e37bc2a418114899
Meredith Black <mblack1190@gmail.com>
2020-04-28 15:09:27 -0500

    display questions and answer choice buttons on screen and start timer

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 var myQuestions = [
     {
     question: "1. Which of these values is NOT considered false?",
@@ -32,23 +33,65 @@ var myQuestions = [
 var startBtn = document.getElementById("start");
 var startScreen = document.getElementById("start-screen");
 var quizScreen = document.getElementById("quiz-screen");
+var endScreen = document.getElementById("end-screen");
+var timerEl = document.getElementById("timer")
 var questionTitle = document.getElementById("question-title");
-
+var choicesEl = document.getElementById("choices")
+var responseEl = document.getElementById("response");
 var Q = 0;
+var time = 60;
+var timerId;
+
+function clockTick() {
+    time--;
+    timerEl.textContent = time;
+}
 
 
 startBtn.addEventListener("click", startGame);
 
 function buildQuestionCard() {
-    var currentQuestion =  myQuestions[Q].question;
-    questionTitle.textContent = currentQuestion;
+    var currentQuestion =  myQuestions[Q];
+    questionTitle.textContent = currentQuestion.question;
 
+    choicesEl.innerHTML = "";
     // loop over our choices and create a button for each one
+    currentQuestion.choices.forEach(function(choice, i) {
+        var buttonEl = document.createElement("button");
+        buttonEl.setAttribute("class", "choice");
+        buttonEl.setAttribute("value", choice)
+
+        buttonEl.textContent = choice;
+
+        buttonEl.onclick = questionClick;       
+
+        choicesEl.appendChild(buttonEl)
+
+    })
 }
 
+function questionClick(){
+   if(this.value !== myQuestions[Q].answer) {
+       responseEl.textContent = "wrong";
+   } else {
+       responseEl.textContent = "right";
+   }
+   Q++;
+   if(Q === myQuestions.length){
+       endQuiz();
+   } else {
+       buildQuestionCard();
+   }
+}
+
+function endQuiz() {
+    quizScreen.setAttribute("class", "hide");
+    endScreen.removeAttribute("class", "hide");
+}
 function startGame() {
     startScreen.setAttribute("class", "hide");
     quizScreen.removeAttribute("class", "hide");
 
     buildQuestionCard();
+    timerId = setInterval(clockTick, 1000)
 }
